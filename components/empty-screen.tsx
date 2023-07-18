8888f06b58bf414fb06d38466bf01da46a43201b
Joseph Petrillo <jpetrillo119@gmail.com>
2023-07-18 11:26:44 -0400

    basic setup complete

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,21 +1,17 @@
 import { UseChatHelpers } from 'ai/react'
 
 import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/external-link'
 import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'What is Citizens Bank?',
+    message: `You are Cleo, a super friendly personal assistant for Citizens Bank. You are helping a customer who wants to know more about Citizens Bank and what it is. \n`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
-  },
-  {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Do you offer mortgages?',
+    message:
+      'You are Cleo, a super friendly personal assistant for Citizens Bank. You are helping a customer who wants to know more about Citizens Bank mortgages and your offerings. \n'
   }
 ]
 
@@ -23,19 +19,13 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-        </h1>
+        <h1 className="mb-2 text-lg font-semibold">Hi, I&apos;m Cleo! 👋</h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          I can answer any questions you may have about Citizens Bank. Ask away!
         </p>
         <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
+          You can enter your own questions at the bottom of this page, or try
+          the following examples to help get you started.
         </p>
         <div className="mt-4 flex flex-col items-start space-y-2">
           {exampleMessages.map((message, index) => (
--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,11 +1,13 @@
-import { JetBrains_Mono as FontMono, Inter as FontSans } from 'next/font/google'
+import { Fira_Sans as FontSans, Fira_Mono as FontMono } from 'next/font/google'
 
 export const fontSans = FontSans({
   subsets: ['latin'],
-  variable: '--font-sans'
+  variable: '--font-sans',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 })
 
 export const fontMono = FontMono({
   subsets: ['latin'],
-  variable: '--font-mono'
+  variable: '--font-mono',
+  weight: ['400', '500', '700']
 })
